1b85fa5c663dc083e52bbc214da7cb7d02a842d0
Pablo Isla Casares <itc.import.export@gmail.com>
2020-08-07 11:48:16 -0500

    segundo Commit

---
+++ src/main/java/com/pabloisla/demo/modelos/ClienteTitular.java
@@ -0,0 +1,10 @@
+package com.pabloisla.demo.modelos;
+
+public class ClienteTitular {
+
+    private int idCliente;
+    private String nombreApellido;
+    private String celular;
+    private String email;
+
+}
--- src/main/java/com/pabloisla/demo/modelos/Empresa.java
+++ src/main/java/com/pabloisla/demo/modelos/Empresa.java
@@ -1,14 +1,21 @@
 package com.pabloisla.demo.modelos;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
 
 public class Empresa {
 
+    @Id
     private int nit;
+    @Column
     private String razonSocial;
     private String telefono;
     private String email;
